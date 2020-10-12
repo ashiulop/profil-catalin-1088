78fb4a151a8b1545b942e1eea06074e1ef1dc80a
ashiulop <catalin.vornicu707@gmail.com>
2020-10-12 15:54:07 +0300

    profile page

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use('/', express.static('frontend'))
- 
-app.get('/hello', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(8080)
+app.use("/", express.static("frontend"));
+
+app.get("/hello", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(8080, () => console.log("The server is running on localhost:8080") );
